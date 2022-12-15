15b1f119843a99d449f3802c3ef4b97409d7ff2d
Nathanyel Sandi <nathanyel.sandi@cobalt.io>
2022-12-15 19:26:05 +0100

    Adjust Folders

--- bank-coding-challenge/src/main/kotlin/com/finmid/bank/repository/AccountRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.finmid.bank.repository
-
-import com.finmid.bank.model.Account
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface AccountRepository : CrudRepository<Account, Long>
